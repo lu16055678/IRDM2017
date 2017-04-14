49a9347807aba9239d83d1d6afb1621c183d71ce
Mingmin Lu <mingminlu@eduroam-int-dhcp-97-160-24.ucl.ac.uk>
2017-04-14 11:34:08 +0100

    search engine framwork complete

--- src/Index/SearchFiles.java
+++ src/Index/SearchFiles.java
@@ -1,4 +1,5 @@
 package Index;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -12,17 +13,136 @@ import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.FSDirectory;
+
 public class SearchFiles {
 	private static final String INDEXPATH = "index";
 	private static final String DOCPATH = "pageText";
-	String field = "contents";
-	public void Search(){
-		
+	private String field = "contents";
+	private boolean raw = false;
+	private int hitsPerpage = 50;
+
+	public void Search(String queryString) throws IOException, ParseException {
+		IndexReader reader = DirectoryReader.open(FSDirectory.open(Paths.get(INDEXPATH)));
+		IndexSearcher searcher = new IndexSearcher(reader);
+		Analyzer analyzer = new StandardAnalyzer();
+
+		BufferedReader in = null;
+		in = Files.newBufferedReader(Paths.get(DOCPATH), StandardCharsets.UTF_8);
+		QueryParser parser = new QueryParser(field, analyzer);
+		queryString = queryString.trim();
+		Query query = parser.parse(queryString);
+		while (true) {
+			System.out.println("Searching for: " + query.toString(field));
+			doPagingSearch(in, searcher, query, hitsPerpage, raw, DOCPATH == null && queryString == null);
+			if (queryString != null) {
+				break;
+			}
+		}
+		reader.close();
+	}
+
+	public static void doPagingSearch(BufferedReader in, IndexSearcher searcher, Query query, int hitsPerPage,
+			boolean raw, boolean interactive) throws IOException {
+
+		// Collect enough docs to show 5 pages
+		TopDocs results = searcher.search(query, 5 * hitsPerPage);
+		ScoreDoc[] hits = results.scoreDocs;
+
+		int numTotalHits = results.totalHits;
+		System.out.println(numTotalHits + " total matching documents");
+
+		int start = 0;
+		int end = Math.min(numTotalHits, hitsPerPage);
+
+		while (true) {
+			if (end > hits.length) {
+				System.out.println("Only results 1 - " + hits.length + " of " + numTotalHits
+						+ " total matching documents collected.");
+				System.out.println("Collect more (y/n) ?");
+				String line = in.readLine();
+				if (line.length() == 0 || line.charAt(0) == 'n') {
+					break;
+				}
+
+				hits = searcher.search(query, numTotalHits).scoreDocs;
+			}
+
+			end = Math.min(hits.length, start + hitsPerPage);
+
+			for (int i = start; i < end; i++) {
+				if (raw) { // output raw format
+					System.out.println("doc=" + hits[i].doc + " score=" + hits[i].score);
+					continue;
+				}
+
+				Document doc = searcher.doc(hits[i].doc);
+				String path = doc.get("path");
+				if (path != null) {
+					System.out.println((i + 1) + ". " + path);
+					String title = doc.get("title");
+					if (title != null) {
+						System.out.println("   Title: " + doc.get("title"));
+					}
+				} else {
+					System.out.println((i + 1) + ". " + "No path for this document");
+				}
+
+			}
+
+			if (!interactive || end == 0) {
+				break;
+			}
+
+			if (numTotalHits >= end) {
+				boolean quit = false;
+				while (true) {
+					System.out.print("Press ");
+					if (start - hitsPerPage >= 0) {
+						System.out.print("(p)revious page, ");
+					}
+					if (start + hitsPerPage < numTotalHits) {
+						System.out.print("(n)ext page, ");
+					}
+					System.out.println("(q)uit or enter number to jump to a page.");
+
+					String line = in.readLine();
+					if (line.length() == 0 || line.charAt(0) == 'q') {
+						quit = true;
+						break;
+					}
+					if (line.charAt(0) == 'p') {
+						start = Math.max(0, start - hitsPerPage);
+						break;
+					} else if (line.charAt(0) == 'n') {
+						if (start + hitsPerPage < numTotalHits) {
+							start += hitsPerPage;
+						}
+						break;
+					} else {
+						int page = Integer.parseInt(line);
+						if ((page - 1) * hitsPerPage < numTotalHits) {
+							start = (page - 1) * hitsPerPage;
+							break;
+						} else {
+							System.out.println("No such page");
+						}
+					}
+				}
+				if (quit)
+					break;
+				end = Math.min(numTotalHits, start + hitsPerPage);
+			}
+		}
+	}
+	public static void main(String args[]) throws IOException, ParseException{
+		SearchFiles search = new SearchFiles();
+		search.Search("Computer Science");
 	}
 }

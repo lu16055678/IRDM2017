2b54b49901b63a265c52ab390e077419e127acc3
Vicky_09 <vicky.liu1009@gmail.com>
2017-04-15 17:46:29 +0100

    Add files via upload
    Ranking

---
+++ SearchFiles.java
@@ -0,0 +1,184 @@
+package Index;
+/**
+ * This code was based on the source code on tutorials of Apache Lucene website.
+ * It was modified by @author mingminlu.
+ */
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+import java.io.FileReader;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.FSDirectory;
+
+import Rank.TFIDFCalculator;
+
+public class SearchFiles {
+	private static final String INDEXPATH = "index";
+	private static final String DOCPATH = "pageText";
+	private String field = "contents";
+	private int hitsPerpage = 100;
+	private static List<String> docList = new LinkedList<String>();
+
+	public void Search(String queryString) throws IOException, ParseException {
+		IndexReader reader = DirectoryReader.open(FSDirectory.open(Paths.get(INDEXPATH)));
+		IndexSearcher searcher = new IndexSearcher(reader);
+		Analyzer analyzer = new StandardAnalyzer();
+		BufferedReader in = null;
+		in = Files.newBufferedReader(Paths.get(DOCPATH), StandardCharsets.UTF_8);
+		QueryParser parser = new QueryParser(field, analyzer);
+		queryString = queryString.trim();
+		Query query = parser.parse(queryString);
+		while (true) {
+			System.out.println("Searching for: " + query.toString(field));
+			doPagingSearch(in, searcher, query, hitsPerpage, DOCPATH == null && queryString == null);
+			if (queryString != null) {
+				break;
+			}
+		}
+		reader.close();
+	}
+
+	public static void doPagingSearch(BufferedReader in, IndexSearcher searcher, Query query, int hitsPerPage,
+			boolean interactive) throws IOException {
+		// Collect enough docs to show 5 pages
+		TopDocs results = searcher.search(query, 5 * hitsPerPage);
+		ScoreDoc[] hits = results.scoreDocs;
+		int numTotalHits = results.totalHits;
+		System.out.println(numTotalHits + " total matching documents");
+		System.out.println();
+		int start = 0;
+		int end = Math.min(numTotalHits, hitsPerPage);
+		while (true) {
+			if (end > hits.length) {
+				System.out.println("Only results 1 - " + hits.length + " of " + numTotalHits
+						+ " total matching documents collected.");
+				System.out.println("Collect more (y/n) ?");
+				String line = in.readLine();
+				if (line.length() == 0 || line.charAt(0) == 'n') {
+					break;
+				}
+
+				hits = searcher.search(query, numTotalHits).scoreDocs;
+			}
+
+			end = Math.min(hits.length, start + hitsPerPage);
+			
+			for (int i = start; i < end; i++) {
+				
+				Document doc = searcher.doc(hits[i].doc);
+				
+				String path = doc.get("path");
+				if (path != null) {
+					
+					//File file = new File(path);
+					String fileContent = readFileContent(path);
+					docList.add(i,fileContent);
+					System.out.println((i + 1) + ". " + path);
+					String title = doc.get("title");
+					if (title != null) {
+						System.out.println("   Title: " + doc.get("title"));
+					}
+				} else {
+					System.out.println((i + 1) + ". " + "No path for this document");
+				}
+
+			}
+
+			if (!interactive || end == 0) {
+				break;
+			}
+
+			if (numTotalHits >= end) {
+				boolean quit = false;
+				while (true) {
+					System.out.print("Press ");
+					if (start - hitsPerPage >= 0) {
+						System.out.print("(p)revious page, ");
+					}
+					if (start + hitsPerPage < numTotalHits) {
+						System.out.print("(n)ext page, ");
+					}
+					System.out.println("(q)uit or enter number to jump to a page.");
+
+					String line = in.readLine();
+					if (line.length() == 0 || line.charAt(0) == 'q') {
+						quit = true;
+						break;
+					}
+					if (line.charAt(0) == 'p') {
+						start = Math.max(0, start - hitsPerPage);
+						break;
+					} else if (line.charAt(0) == 'n') {
+						if (start + hitsPerPage < numTotalHits) {
+							start += hitsPerPage;
+						}
+						break;
+					} else {
+						int page = Integer.parseInt(line);
+						if ((page - 1) * hitsPerPage < numTotalHits) {
+							start = (page - 1) * hitsPerPage;
+							break;
+						} else {
+							System.out.println("No such page");
+						}
+					}
+				}
+				if (quit)
+					break;
+				end = Math.min(numTotalHits, start + hitsPerPage);
+			}
+		}
+	}
+	private static String readFileContent(String fileName) throws IOException {
+		  File file = new File(fileName);
+		  
+		  BufferedReader bf = new BufferedReader(new FileReader(file));
+		  
+		  String content = "";
+		  StringBuilder sb = new StringBuilder();
+		  
+		  while(content != null){
+		   content = bf.readLine();
+		   
+		   if(content == null){
+		    break;
+		   }
+		   
+		   sb.append(content.trim());
+		  }
+		  
+		bf.close();
+		  return sb.toString();
+		 }
+
+
+	public static void main(String args[]) throws IOException, ParseException {
+		List<List<String>> documents= new LinkedList<List<String>>();
+		
+		SearchFiles search = new SearchFiles();
+		System.out.println("Enter a word to search:");
+		String input = new Scanner(System.in).nextLine();
+		search.Search(input);
+		documents.add(docList);
+		TFIDFCalculator calculator = new TFIDFCalculator();
+        double tfidf = calculator.tfIdf(docList, documents, "ipsum");
+        System.out.println("TF-IDF (ipsum) = " + tfidf);
+	}
+}
